dd45973fac57d6bda4e3334531faed2894d10568
Frank James <frank.a.james@gmail.com>
2015-08-09 20:30:05 +0100

    moving block size to toplevel

--- device.lisp
+++ device.lisp
@@ -10,9 +10,6 @@
 
 (in-package #:device)
 
-(defstruct device 
-  handle geometry)
-
 ;; access 
 ;;#define GENERIC_READ                     (0x80000000L)
 ;;#define GENERIC_WRITE                    (0x40000000L)
@@ -34,36 +31,40 @@
 ;; flags
 ;; FILE_ATTRIBUTE_NORMAL 128
 
-#|  
-typedef enum _MEDIA_TYPE {
-    Unknown,                // Format is unknown
-    F5_1Pt2_512,            // 5.25", 1.2MB,  512 bytes/sector
-    F3_1Pt44_512,           // 3.5",  1.44MB, 512 bytes/sector
-    F3_2Pt88_512,           // 3.5",  2.88MB, 512 bytes/sector
-    F3_20Pt8_512,           // 3.5",  20.8MB, 512 bytes/sector
-    F3_720_512,             // 3.5",  720KB,  512 bytes/sector
-    F5_360_512,             // 5.25", 360KB,  512 bytes/sector
-    F5_320_512,             // 5.25", 320KB,  512 bytes/sector
-    F5_320_1024,            // 5.25", 320KB,  1024 bytes/sector
-    F5_180_512,             // 5.25", 180KB,  512 bytes/sector
-    F5_160_512,             // 5.25", 160KB,  512 bytes/sector
-    RemovableMedia,         // Removable media other than floppy
-    FixedMedia,             // Fixed hard disk media
-    F3_120M_512,            // 3.5", 120M Floppy
-    F3_640_512,             // 3.5" ,  640KB,  512 bytes/sector
-    F5_640_512,             // 5.25",  640KB,  512 bytes/sector
-    F5_720_512,             // 5.25",  720KB,  512 bytes/sector
-    F3_1Pt2_512,            // 3.5" ,  1.2Mb,  512 bytes/sector
-    F3_1Pt23_1024,          // 3.5" ,  1.23Mb, 1024 bytes/sector
-    F5_1Pt23_1024,          // 5.25",  1.23MB, 1024 bytes/sector
-    F3_128Mb_512,           // 3.5" MO 128Mb   512 bytes/sector
-    F3_230Mb_512,           // 3.5" MO 230Mb   512 bytes/sector
-    F8_256_128,             // 8",     256KB,  128 bytes/sector
-    F3_200Mb_512,           // 3.5",   200M Floppy (HiFD)
-    F3_240M_512,            // 3.5",   240Mb Floppy (HiFD)
-    F3_32M_512              // 3.5",   32Mb Floppy
-} MEDIA_TYPE, *PMEDIA_TYPE;
-|#
+;; typedef enum _MEDIA_TYPE {
+;;     Unknown,                // Format is unknown
+;;     F5_1Pt2_512,            // 5.25", 1.2MB,  512 bytes/sector
+;;     F3_1Pt44_512,           // 3.5",  1.44MB, 512 bytes/sector
+;;     F3_2Pt88_512,           // 3.5",  2.88MB, 512 bytes/sector
+;;     F3_20Pt8_512,           // 3.5",  20.8MB, 512 bytes/sector
+;;     F3_720_512,             // 3.5",  720KB,  512 bytes/sector
+;;     F5_360_512,             // 5.25", 360KB,  512 bytes/sector
+;;     F5_320_512,             // 5.25", 320KB,  512 bytes/sector
+;;     F5_320_1024,            // 5.25", 320KB,  1024 bytes/sector
+;;     F5_180_512,             // 5.25", 180KB,  512 bytes/sector
+;;     F5_160_512,             // 5.25", 160KB,  512 bytes/sector
+;;     RemovableMedia,         // Removable media other than floppy
+;;     FixedMedia,             // Fixed hard disk media
+;;     F3_120M_512,            // 3.5", 120M Floppy
+;;     F3_640_512,             // 3.5" ,  640KB,  512 bytes/sector
+;;     F5_640_512,             // 5.25",  640KB,  512 bytes/sector
+;;     F5_720_512,             // 5.25",  720KB,  512 bytes/sector
+;;     F3_1Pt2_512,            // 3.5" ,  1.2Mb,  512 bytes/sector
+;;     F3_1Pt23_1024,          // 3.5" ,  1.23Mb, 1024 bytes/sector
+;;     F5_1Pt23_1024,          // 5.25",  1.23MB, 1024 bytes/sector
+;;     F3_128Mb_512,           // 3.5" MO 128Mb   512 bytes/sector
+;;     F3_230Mb_512,           // 3.5" MO 230Mb   512 bytes/sector
+;;     F8_256_128,             // 8",     256KB,  128 bytes/sector
+;;     F3_200Mb_512,           // 3.5",   200M Floppy (HiFD)
+;;     F3_240M_512,            // 3.5",   240Mb Floppy (HiFD)
+;;     F3_32M_512              // 3.5",   32Mb Floppy
+;; } MEDIA_TYPE, *PMEDIA_TYPE;
+
+(defstruct device 
+  handle 
+  geometry
+  bytes
+  size)
 
 
 (defun open-device-handle (name &key (access #x10000000) (mode 0) (disposition 3) (flags 0))
@@ -138,7 +139,9 @@ typedef enum _MEDIA_TYPE {
       (handler-case 
 	  (let ((geometry (get-disk-geometry handle)))
 	    (make-device :handle handle
-			 :geometry geometry))
+			 :geometry geometry
+			 :bytes (geometry-bytes geometry)
+			 :size (geometry-size geometry)))
 	(error (e)
 	  (pounds::%close-handle handle)
 	  (error e))))))
@@ -163,7 +166,7 @@ typedef enum _MEDIA_TYPE {
 	   (type integer block)
 	   (type (vector (unsigned-byte 8)) sequence))
   (let ((count (- (or end (length sequence)) start))
-	(offset (* block (geometry-bytes (device-geometry device)))))
+	(offset (* block (device-bytes device))))
     (pounds::read-file (device-handle device)
 		       sequence
 		       offset
@@ -176,7 +179,7 @@ typedef enum _MEDIA_TYPE {
 	   (type integer block)
 	   (type (unsigned-byte 8) value))
   (let ((length (geometry-bytes (device-geometry device)))
-	(offset (* block (geometry-bytes (device-geometry device)))))
+	(offset (* block (device-bytes device))))
     (with-foreign-objects ((buffer :uint8 length)
 			   (nbytes :uint32)
 			   (overlapped '(:struct pounds::overlapped)))
@@ -197,17 +200,18 @@ typedef enum _MEDIA_TYPE {
 				      length
 				      nbytes
 				      overlapped)))
-	(if res
-	    nil
-	    (pounds::get-last-error))))))
+	(unless res 
+	  (pounds::get-last-error))))))
 
 (defun move-block (device new-block old-block)
+  (declare (type device device)
+	   (type integer new-block old-block))
   (let ((length (geometry-bytes (device-geometry device))))
     (with-foreign-objects ((buffer :uint8 length)
 			   (nbytes :uint32)
 			   (overlapped '(:struct pounds::overlapped)))
       ;; start by reading the old block
-      (let ((offset (* old-block (geometry-bytes (device-geometry device)))))
+      (let ((offset (* old-block length)))
 	(multiple-value-bind (offset-low offset-high) (pounds::split-offset offset)
 	  (setf (foreign-slot-value overlapped '(:struct pounds::overlapped)
 				    'pounds::offset)
@@ -215,13 +219,15 @@ typedef enum _MEDIA_TYPE {
 		(foreign-slot-value overlapped '(:struct pounds::overlapped)
 				    'pounds::offset-high)
 		offset-high)
-	  (pounds::%read-file (device-handle device)
-			      buffer 
-			      length 
-			      nbytes
-			      overlapped)))
+	  (let ((res (pounds::%read-file (device-handle device)
+					 buffer 
+					 length 
+					 nbytes
+					 overlapped)))
+	    (unless res
+	      (pounds::get-last-error)))))
       ;; now write it back out to the new location
-      (let ((offset (* new-block (geometry-bytes (device-geometry device)))))
+      (let ((offset (* new-block length)))
 	(multiple-value-bind (offset-low offset-high) (pounds::split-offset offset)
 	  (setf (foreign-slot-value overlapped '(:struct pounds::overlapped)
 				    'pounds::offset)
@@ -229,11 +235,13 @@ typedef enum _MEDIA_TYPE {
 		(foreign-slot-value overlapped '(:struct pounds::overlapped)
 				    'pounds::offset-high)
 		offset-high)
-	  (pounds::%write-file (device-handle device)
-			       buffer 
-			       length 
-			       nbytes
-			       overlapped))))))
+	  (let ((res (pounds::%write-file (device-handle device)
+					  buffer 
+					  length 
+					  nbytes
+					  overlapped)))
+	    (unless res
+	      (pounds::get-last-error))))))))
 
 
 	  
